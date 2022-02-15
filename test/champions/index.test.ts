0938993c7226423eb575aae1869f063a051ff1fc
B4129 <B4129@oic.jp>
2022-02-16 01:58:27 +0900

    fix: git ignore

--- test/champions/index.test.ts
+++ test/champions/index.test.ts
@@ -1,6 +1,6 @@
 import {lol} from '../../src';
 
 test('指定のスキンURLの取得', () => {
-    const url = lol.champion('Poppy').skin(1);
-    expect( url ).toBe('https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Poppy_1.jpg');
+  const url = lol.champion('Poppy').skin(1);
+  expect( url ).toBe('https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Poppy_1.jpg');
 });
